04b953f648659e41fb0e01057aa9c756647a6538
SupriyaSS1998 <supriyass.xworks.gmail.com>
2022-11-10 04:07:26 -0800

    commiting Arrays files

---
+++ Arrays/src/com/xworkz/Ascending/Ascending.java
@@ -0,0 +1,18 @@
+package com.xworkz.Ascending;
+public class Ascending  {
+
+  public static void main(String[] args) {
+	  
+	  int nums[] = new int[5];
+	  nums[0] =123;
+	  nums[1] =1234;
+	  nums[2] =12345;
+	  nums[3] =123456;
+	  nums[4] =1234567;
+	  
+	  System.out.println("Ascending Order");
+	  System.out.println("Arrays.toString(nums)");
+	  
+	   
+  }
+}
