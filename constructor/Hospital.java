767f651776fa8d28d0e05a56767a83d85e181e42
SupriyaSS1998 <supriyass.xworks.gmail.com>
2022-11-14 02:59:05 -0800

    commiting constructor files

---
+++ constructor/Hospital.java
@@ -0,0 +1,60 @@
+package com.xworkz.Hospital.app.Hospital;
+
+public class Hospital {
+	
+	
+   
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//oldPatientName = "Guna";
+	//newPatientName = "Guna Sarkar";
+
+public boolean updatePatientNames(String oldPatientNames, newPatientNames)
+	{
+		System.out.print("inside updatePatientNames");
+		for(int i = 0; i< patientNames.length;i++);
+		  //Ganesh == Guna
+		 if (patientNames) {
+		    patientNames[i]  == oldPatientName;
+		  	patientNames[i] == newPatientName;
+			return true;
+		 }
+		 
+		 else
+			 
+	}
+}
+	return false;
+	
+
+		
+
+
+	
+      
+
