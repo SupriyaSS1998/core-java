6c90e69300cdc4b5ba2176dc79017a35fef29221
SupriyaSS1998 <supriyass.xworks.gmail.com>
2022-12-01 04:18:08 -0800

    commiting projects files

---
+++ projects/Arrays/src/com/xworkz/Ascending/Descending.java
@@ -0,0 +1,21 @@
+package com.xworkz.Ascending;
+
+public class Descending {
+
+public static void main(String[] args) {
+		  
+		  int nums[] = new int[5];
+		  nums[0] = 765321;
+		  nums[1] = 65321;
+		  nums[2] = 5321;
+		  nums[3] = 4321;
+		  nums[4] = 321;
+		  
+		  System.out.println("Descending Order");
+		  System.out.println("Arrays.toString(nums)");
+		  
+		   
+	  }
+	}
+
+
